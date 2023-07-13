309147e619c4700f4b52c8aff5d6310aa0ebc324
unknown <samtisme@gmail.com>
2023-07-12 19:53:03 -0700

    wrapped response data in try block vs code was expecting a try token

--- assets/script.js
+++ assets/script.js
@@ -15,7 +15,7 @@ searchForm.addEventListener('submit', function (e) {
     }
 });
 
- searchHistory.addEventListener('click', function (e) {
+searchHistory.addEventListener('click', function (e) {
     const cityName = e.target.textContent;
     if (cityName) {
         getWeather(cityName);
@@ -25,32 +25,39 @@ searchForm.addEventListener('submit', function (e) {
 function getWeather(city) {
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=imperial`;
 
-    fetch(apiUrl)
-    .then(response => response.json())
-    .then(data => {
-        const forecastData = data.list;
-        
-        forecast.innerHTML = '<h2>5-Day Forecast</h2>';
-        for (let i = 0; i < forecastData.length; i += 8) {
-            const { dt_txt, main: { temp, humidity } } = forecastData[i];
-            const forecastDate = new Date(dt * 1000).toLocaleDateString('en-US');
-            const iconUrl = `https://openweathermap.org/img/wn/${forecastData[i].weather[0].icon}.png`;
+    try {
+        fetch(apiUrl)
+            .then(response => response.json())
+            .then(data => {
+                const forecastData = data.list;
 
-            const forecastItem = document.createElement('div');
-            forecastItem.classList.add('forecast-item');
-            forecastItem.innerHTML = `
+                forecast.innerHTML = '<h2>5-Day Forecast</h2>';
+                for (let i = 0; i < forecastData.length; i += 8) {
+                    const { dt_txt, main: { temp, humidity } } = forecastData[i];
+                    const forecastDate = new Date(dt * 1000).toLocaleDateString('en-US');
+                    const iconUrl = `https://openweathermap.org/img/wn/${forecastData[i].weather[0].icon}.png`;
+
+                    const forecastItem = document.createElement('div');
+                    forecastItem.classList.add('forecast-item');
+                    forecastItem.innerHTML = `
             <p>${forecastDate}</p>
             <img src="${iconUrl}" alt="${forecastData[i].weather[0].description}" />
             <p>Temperature: ${Math.round(main.temp)} &deg;F</p>
             <p>Humidity: ${humidity}%</p>
             <p>Wind Speed: ${wind.speed} MPH</p>
             `;
-            forecast.appendChild(forecastItem);
-        }
-    });
-    .catch(err => {
-        console.error("Error: ", error);
-    });
+                    forecast.appendChild(forecastItem);
+                }
+            });
+    } catch (error) {
+        console.error('Error:', error);
+    }
+
+
+    function addToSearchHistory(cityName) {
+        const searchHistoryItem = document.createElement('li');
+        searchHistoryItem.classList.add('search-history-item');
+        searchHistoryItem.textContent = cityName;
+        searchHistory.appendChild(searchHistoryItem);
+    }
 }
-
-
